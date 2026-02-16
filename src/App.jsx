2912ba01c8995ac1d93a415b41bb910e73530b64
Ico24-t <iconsu@gmail.com>
2026-02-16 22:34:13 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import { FormProvider } from './contexts/FormContext'
 import MainForm from './pages/MainForm'
 import BillUpload from './pages/BillUpload'
+import ThankYou from './pages/ThankYou'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
@@ -16,6 +17,7 @@ function App() {
             <Routes>
               <Route path="/" element={<MainForm />} />
               <Route path="/bolletta" element={<BillUpload />} />
+              <Route path="/grazie" element={<ThankYou />} />
               <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
           </main>
